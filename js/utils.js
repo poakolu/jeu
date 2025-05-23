8721b66fad121a4f17d4076d677686fa6992216a
poakolu <84971392+poakolu@users.noreply.github.com>
2025-05-23 23:38:23 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,6 @@
 // utils.js
 
-// Exemple : fonction pour afficher un message dans une zone dédiée (à créer plus tard)
+// Exemple : fonction pour afficher un message dans une zone dédiée (à améliorer plus tard)
 function showMessage(message) {
   console.log(message);
 }
-
